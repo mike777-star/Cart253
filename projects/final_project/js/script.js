776a7f2806c197b08d361297a67caba46b28e9eb
Michael Sarlos <michaelsarlos@Michaels-MacBook-Pro.local>
2019-12-08 18:48:39 -0500

    Commit
    I added my functions to begin and end the game in the script. This involved adding a start and end screen. The start screen must be clicked to start the game.

--- projects/final_project/js/script.js
+++ projects/final_project/js/script.js
@@ -14,6 +14,14 @@ let lazer;
 let blocks;
 let aliens;
 
+//Defining my start and end screen images
+let starterImage;
+let enderImage;
+
+//Defining my when game begins and ends
+let gameBegin = true;
+let gameOver = false;
+
 //setting up my image files even though they undefined
 function preload() {
   player = loadImage('assets/images/spaceman.png');
@@ -26,7 +34,9 @@ function preload() {
 
 function setup() {
 
-  createCanvas(500, 500);
+//Resized the canvas to be wider. I forgot to do that when I changed...
+//... when I changed my elements positions and size
+  createCanvas(500, 300);
   rectMode(CENTER);
   noStroke();
   fill(fgColor);
@@ -43,29 +53,60 @@ function setup() {
 
 function draw() {
 
-//I am calling my array groups alongside their functions involving the player
+  //Calling the title screen when the game loads
+  if (gameOver == false && gameBegin == true){
+    // image(enderImage, 0, 0, width, height);
+    title.display();
+    console.log(gameBegin);
+    return;
 
-  for (let i = 0; i < obstacleGroup.length; i++) {
-      obstacleGroup[i].move();
-      obstacleGroup[i].display();
-      player.handleBlocking(obstacleGroup[i]);
+    }
+  //Calling the GameOver screen when the game ends
+      if (player.health <= 0){
+    gameOver = true;
+    fill(255, 0, 0);
+    textSize(50);
+    // image(enderImage, 0, 0, width, height)
+    text('GameOver' ,300,550);
+    }
+
+      if (gameOver == false) {
+
+  //I am calling my array groups alongside their functions involving the player
+
+    for (let i = 0; i < obstacleGroup.length; i++) {
+        obstacleGroup[i].move();
+        obstacleGroup[i].display();
+        player.handleBlocking(obstacleGroup[i]);
+
+      }
+
+      for (let i = 0; i < enemyGroup.length; i++) {
+      enemyGroup[i].move();
+      enemyGroup[i].display();
+      //For when I add the blaster
+      player.handleZap(enemyGroup[i]);
+      player.handleEating(enemyGroup[i]);
+      }
+
+
+  //My player's function
+      player.display();
+      player.move();
+      player.handleInput();
+      player.handleBlocking(blocks);
+      player.handleZap(aliens);
+      player.handleEating(aliens);
 
     }
 
-    for (let i = 0; i < enemyGroup.length; i++) {
-    enemyGroup[i].move();
-    enemyGroup[i].display();
-    //For when I add the blaster
-    player.handleZap(enemyGroup[i]);
-    player.handleEating(enemyGroup[i]);
+
+  }
+
+  //Defining the function which lets the player click the starter screen...
+  //...to trigger the beginning of the game
+
+  function mouseClicked(){
+    if (mouseX > 0 && mouseY > 0){
+      gameBegin = false;
     }
-
-
-//My player's function
-    player.display();
-    player.move();
-    player.handleInput();
-    player.handleBlocking(blocks);
-    player.handleZap(aliens);
-    player.handleEating(aliens);
-}
